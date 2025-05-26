1cfa5dd6488d2f8407cacb7fa8d0dd02b7dbd5b5
vishalVerma <vishal.verma@alertdriving.com>
2025-05-26 22:36:51 +0530

    P911712: Ignore Header Line

--- build/index.js
+++ build/index.js
@@ -51012,8 +51012,11 @@
                                       switch (fileType) {
                                           case "csv":
                                               papaparse_min.parse(bstr.toString(), {
-                                                  header: true,
-                                                  skipEmptyLines: 'greedy',
+                                                  beforeFirstChunk: function (chunk) {
+                                                      var lines = chunk.split('\n');
+                                                      lines.splice(0, 1); // Remove the first n lines
+                                                      return lines.join('\n');
+                                                  },
                                                   complete: function (results) {
                                                       var csvData = results.data;
                                                       var rows = csvData.filter(isNotBlankRow).map(function (row, index) { return ({ index: index, values: row }); });
